2c13f9f2ea12ec907495aab9f84ff21496d642b6
Valentin Kofman <kvmisis@gmail.com>
2016-09-12 11:21:43 +0300

    fix script

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
+#!/bin/bash
 read -p "Install oh-my-zsh? (yes/no): " prompt
 if [ "$prompt" == "yes" ]; then
-  wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -
+  sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 fi
