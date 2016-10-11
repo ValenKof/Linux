5d0d0acb74d30eee2becf1085f91a79262d2f625
Valentin Kofman <kvmisis@gmail.com>
2016-10-11 13:00:10 +0300

    copy themes after install

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -2,4 +2,7 @@
 read -p "Install oh-my-zsh? (yes/no): " prompt
 if [ "$prompt" == "yes" ]; then
   sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+  for THEME in valenkof-user valenkof-root; do
+    cp $(dirname $0)/$THEME.zsh-theme ~/.oh-my-zsh/themes/
+  done
 fi
